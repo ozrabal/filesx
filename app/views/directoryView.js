223f998f4d75482d04a95928d5aa4e103287b14b
Piotr <ozrabal@gmail.com>
2016-09-16 14:03:35 +0200

    rename directories

--- app/views/directoryView.js
+++ app/views/directoryView.js
@@ -8,6 +8,11 @@ define([
         tagName: 'li',
         template: _.template(directoryView),
 
+        events: {
+            'click .btn-edit': 'edit',
+            'click .btn-save': 'update', //todo on hit enter too
+        },
+
         initialize: function(){
 
             this.model.on('change', this.render, this);
@@ -15,6 +20,21 @@ define([
         },
         render: function(){
             this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        },
+
+        edit: function(){
+            //todo refactor /find(.name) into constructor
+            this.$el.addClass('active').find('.name').focus();
+            return this;
+        },
+
+        update: function(){
+            var value = this.$el.find('.name').val();
+            if (value) {
+                this.model.save({name: value});
+            }
+            this.$el.removeClass('active');
             return this;
         }
     })
